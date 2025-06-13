60f1b5dc58ac48f0d036447917fb85f0da42d962
rtrinu <rbinu2004@gmail.com>
2025-06-13 19:20:41 +0100

    customise button and box fade away with player

--- static/js/hideplayer.js
+++ static/js/hideplayer.js
@@ -1,13 +1,18 @@
-const div = document.getElementById("player_data");
+const player = document.getElementById("player_data");
+const customise = document.getElementById("customise");
 let hideTimeout;
 
 function resetHidePlayer(){
     clearTimeout(hideTimeout);
-    div.classList.remove("hidden");
+    player.classList.remove("hidden");
+    customise.classList.remove("hidden");
 
     hideTimeout = setTimeout(() => {
-        div.classList.add("hidden");
+        player.classList.add("hidden");
+        customise.classList.add("hidden");
     }, 3000);
 }
+
+
 
 document.addEventListener("mousemove", resetHidePlayer);
